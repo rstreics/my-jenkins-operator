063616df6baf63c7ac8834e06016294f726129fc
Antons Kranga <anton@agilestacks.com>
2018-02-09 04:12:34 +0200

    removed docker insecure warning

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -3,13 +3,7 @@
 @Grab(group = 'com.amazonaws', module = 'aws-java-sdk', version = '1.11.119')
 import com.amazonaws.services.ecr.model.*
 import com.amazonaws.services.ecr.*
-
-
-def tempFile() {
-    def f = File.createTempFile("ecr", ".tmp")
-    f.deleteOnExit()
-    return f.absolutePath
-}
+import com.amazonaws.regions.Regions
 
 def call(params = [:], arg, Closure body={}) {
     def argv = [ region: Regions.currentRegion.name ]
@@ -18,26 +12,38 @@ def call(params = [:], arg, Closure body={}) {
     } else {
         argv.region = arg
     }
+    String region = argv.region
+
+
+
 
     def data =  AmazonECRClientBuilder
                     .standard()
-                    .withRegion(argv.region)
+                    .withRegion(region)
+                    .build()
                     .getAuthorizationToken(new GetAuthorizationTokenRequest())
                     .authorizationData.first()
     if (!data) {
         error "I was not able to receive ecr auth data"
     }
-
     def server = data.proxyEndpoint
     def token = data.authorizationToken.decodeBase64()
     def login = new String(token).tokenize(':')
-    def passFile = tempFile()
-    writeFile(text: login.last(), file: passFile, )
+    def tempDir = pwd(tmp: true)
+    def uuid = UUID.randomUUID().toString().replace("-", "").take(5)
+    def filename = "${tempDir}/ecr${uuid}.tmp"
 
-    def returnStatus = sh script: "cat ${passFile} | docker login --username ${login.first()} --password-stdin ${server}",
-                          returnStatus:true
-    if (returnStatus != 0) {
-      error "I was not able to login to ECR {auth.proxyEndpoint}"
+    dir( tempDir ) {
+        try {
+            writeFile(text: login.last(), file: filename)
+            def returnStatus = sh(script: "cat ${filename} | docker login --username ${login.first()} --password-stdin ${server}",
+                    returnStatus: true)
+            if (returnStatus != 0) {
+                error "I was not able to login to ECR {auth.proxyEndpoint}"
+            }
+        } finally {
+            new File(filename).delete()
+        }
     }
 
     if (body != null) {
