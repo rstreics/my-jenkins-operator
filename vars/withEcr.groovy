ed8faba05e2a19532ffd320a0a52d6130eb49f5c
Antons Kranga <anton@agilestacks.com>
2018-02-09 02:11:36 +0200

    moved to default ecr client

--- vars/withEcr.groovy
+++ vars/withEcr.groovy
@@ -1,26 +1,25 @@
 #!/usr/bin/env groovy
 
 @Grab(group = 'com.amazonaws', module = 'aws-java-sdk', version = '1.11.119')
-import com.amazonaws.services.ecr.AmazonECRClientBuilder
-import com.amazonaws.services.ecr.model.GetAuthorizationTokenRequest
-import com.amazonaws.auth.InstanceProfileCredentialsProvider
-import com.amazonaws.regions.Regions
+import com.amazonaws.services.ecr.model.*
+import com.amazonaws.services.ecr.*
 
 def call(params = [:], Closure body) {
-    def region = params?.region ?: Regions.currentRegion.name
+//    def region = params?.region ?: Regions.currentRegion.name
+    def data =  AmazonECRClientBuilder
+                    .defaultClient()
+                    .getAuthorizationToken(new GetAuthorizationTokenRequest())
+                    .authorizationData.first()
+    if (!data) {
+        error "I was not able to receive ecr auth data"
+    }
 
-    def auth = AmazonECRClientBuilder.
-            standard().
-            withCredentials(InstanceProfileCredentialsProvider.getInstance()).
-            withRegion(region).
-            build().
-            getAuthorizationToken(new GetAuthorizationTokenRequest()).
-            authorizationData[0].authorizationToken.decodeBase64()
-    def login = new String(auth).tokenize(':')
+    def endpoint = data.proxyEndpoint
+    def token = data.authorizationToken.decodeBase64()
+    def login = new String(token).tokenize(':')
 
-    def returnStatus = sh(script: "docker login -u ${login[0]} -p ${login[1]} ${auth.proxyEndpoint}",
-                          returnStatus:true)
-
+    def returnStatus = sh script: "docker login -u ${login[0]} -p ${login[1]} ${endpoint}",
+                          returnStatus:true
     if (returnStatus != 0) {
       error "I was not able to login to ECR {auth.proxyEndpoint}"
     }
