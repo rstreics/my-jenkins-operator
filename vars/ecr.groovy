f7bf3b8cbbe92923d5dbe8fdb84a4b620e32d336
Antons Kranga <anton@agilestacks.com>
2018-02-09 02:37:27 +0200

    reworked ecr to not to throw warning

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -4,21 +4,37 @@
 import com.amazonaws.services.ecr.model.*
 import com.amazonaws.services.ecr.*
 
-def call(params = [:], Closure body) {
-//    def region = params?.region ?: Regions.currentRegion.name
+
+def tempFile() {
+    def f = File.createTempFile("ecr", ".tmp")
+    f.deleteOnExit()
+    return f.absolutePath
+}
+
+def call(params = [:], arg, Closure body={}) {
+    def argv = [ region: Regions.currentRegion.name ]
+    if (arg instanceof Map) {
+        argv << arg
+    } else {
+        argv.region = arg
+    }
+
     def data =  AmazonECRClientBuilder
-                    .defaultClient()
+                    .standard()
+                    .withRegion(argv.region)
                     .getAuthorizationToken(new GetAuthorizationTokenRequest())
                     .authorizationData.first()
     if (!data) {
         error "I was not able to receive ecr auth data"
     }
 
-    def endpoint = data.proxyEndpoint
+    def server = data.proxyEndpoint
     def token = data.authorizationToken.decodeBase64()
     def login = new String(token).tokenize(':')
+    def passFile = tempFile()
+    writeFile(text: login.last(), file: passFile, )
 
-    def returnStatus = sh script: "docker login -u ${login[0]} -p ${login[1]} ${endpoint}",
+    def returnStatus = sh script: "cat ${passFile} | docker login --username ${login.first()} --password-stdin ${server}",
                           returnStatus:true
     if (returnStatus != 0) {
       error "I was not able to login to ECR {auth.proxyEndpoint}"
--- vars/temp.groovy
+++ vars/temp.groovy
@@ -1,4 +1,4 @@
-!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 import java.nio.file.*
 
