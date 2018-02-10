b36ca43b7a6137156dec1eb6db0a552407cbd8f3
Antons Kranga <anton@agilestacks.com>
2018-02-10 03:30:18 +0200

    introduced updateGithubCommitStatus

---
+++ vars/updateGithubCommitStatus.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+@GrabResolver(name='releases', root='http://repo.jenkins-ci.org/releases/')
+@Grab(group='org.jenkins-ci.plugins.workflow', module='workflow-support', version='2.18')
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+
+
+private def getRepoURL() {
+    sh(script: 'git config --get remote.origin.url', returnStdout: true).trim()
+}
+
+private def getCommitSha() {
+    sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+}
+
+def call(currentBuild = new RunWrapper($build(), true) ) {
+    def repoUrl   = getRepoURL()
+    def commitSha = getCommitSha()
+
+    step([
+            $class: 'GitHubCommitStatusSetter',
+            reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
+            commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
+            errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+            statusResultSource: [
+                    $class: 'ConditionalStatusResultSource',
+                    results: [
+                            [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: currentBuild.message],
+                            [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: currentBuild.message],
+                            [$class: 'AnyBuildResult', state: 'FAILURE', message: currentBuild.message]
+                    ]
+            ]
+    ])
+}
