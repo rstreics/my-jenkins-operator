be19421d77351524c12b0316b20a12043a41661f
Antons Kranga <anton@agilestacks.com>
2018-02-11 14:40:37 +0200

    typo

--- vars/commitHash.groovy
+++ vars/commitHash.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
 def short() {
-    sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim())
+    sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
 def long() {
-    sh(script: 'git rev-parse --long HEAD', returnStdout: true).trim())
+    sh(script: 'git rev-parse --long HEAD', returnStdout: true).trim()
 }
 
 def call() {
