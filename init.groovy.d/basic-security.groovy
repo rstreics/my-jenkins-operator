c6e0bf936d6f73fcaeef826265dcdfa5674ac597
Antons Kranga <anton@agilestacks.com>
2018-02-23 09:52:08 +0000

    commented security setup... currently rely on octa

--- init.groovy.d/basic-security.groovy
+++ init.groovy.d/basic-security.groovy
@@ -9,22 +9,22 @@ def userRegistered = false
 def log     = Logger.getLogger(this.class.name)
 def jenkins = Jenkins.getInstance()
 
-def adminUser     = System.getenv('ADMIN_USER')     ?: "admin"
-def adminPassword = System.getenv('ADMIN_PASSWORD') ?: "secret"
-jenkins.securityRealm = new HudsonPrivateSecurityRealm(false)
-if (adminUser != null && adminPassword != null) {
-  jenkins.securityRealm.createAccount(adminUser, adminPassword)
-  log.info("Added user: ${adminUser}")
-  userRegistered = true
-}
+// def adminUser     = System.getenv('ADMIN_USER')     ?: "admin"
+// def adminPassword = System.getenv('ADMIN_PASSWORD') ?: "secret"
+// jenkins.securityRealm = new HudsonPrivateSecurityRealm(false)
+// if (adminUser != null && adminPassword != null) {
+//   jenkins.securityRealm.createAccount(adminUser, adminPassword)
+//   log.info("Added user: ${adminUser}")
+//   userRegistered = true
+// }
 
-def saUser     = System.getenv('SERVICE_ACCOUNT_USER')
-def saPassword = System.getenv('SERVICE_ACCOUNT_PASSWORD')
-if (saUser != null && saPassword != null) {
-  jenkins.securityRealm.createAccount(saUser, saPassword)
-  log.info("Added user: ${saUser}")
-  userRegistered = true
-}
+// def saUser     = System.getenv('SERVICE_ACCOUNT_USER')
+// def saPassword = System.getenv('SERVICE_ACCOUNT_PASSWORD')
+// if (saUser != null && saPassword != null) {
+//   jenkins.securityRealm.createAccount(saUser, saPassword)
+//   log.info("Added user: ${saUser}")
+//   userRegistered = true
+// }
 
 if (userRegistered) {
   jenkins.authorizationStrategy = new FullControlOnceLoggedInAuthorizationStrategy()
