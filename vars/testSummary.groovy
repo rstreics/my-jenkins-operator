5450496c06714831ae3f4e1f764d8714d101404c
Antons Kranga <anton@agilestacks.com>
2018-05-02 13:27:01 +0300

    better test summary

--- vars/testSummary.groovy
+++ vars/testSummary.groovy
@@ -2,18 +2,19 @@
 
 @GrabResolver(name='releases', root='http://repo.jenkins-ci.org/releases/')
 @Grab(group='org.jenkins-ci.plugins.workflow', module='workflow-support', version='2.14')
-import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 @Grab(group='org.jenkins-ci.plugins', module='junit', version='1.21')
 import hudson.tasks.test.AbstractTestResultAction
+import hudson.model.Run
+//import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 def call() {
-    def currentBuild = new RunWrapper($build(), true)
-    def testResultAction = currentBuild?.rawBuild?.getAction(AbstractTestResultAction.class)
+    Run build = $build()
+    def testResultAction = build.getAction(AbstractTestResultAction)
 
     if (testResultAction != null) {
-        def total = testResultAction.getTotalCount()
-        def failed = testResultAction.getFailCount()
-        def skipped = testResultAction.getSkipCount()
+        def total = testResultAction?.getTotalCount() ?: 0
+        def failed = testResultAction?.getFailCount() ?: 0
+        def skipped = testResultAction?.getSkipCount() ?: 0
         def passed = total - failed - skipped
 
        return "Passed: ${passed}, Failed: ${failed}, Skipped ${skipped}"
