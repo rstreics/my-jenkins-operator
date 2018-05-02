384acd805279b0b4a07d944736afd2eb8e42bfa6
Antons Kranga <anton@agilestacks.com>
2018-05-02 13:32:23 +0300

    added url to test report

--- vars/testSummary.groovy
+++ vars/testSummary.groovy
@@ -7,6 +7,18 @@ import hudson.tasks.test.AbstractTestResultAction
 import hudson.model.Run
 //import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
+def withUrl() {
+    Run build = $build()
+    def testResultAction = build.getAction(AbstractTestResultAction)
+    if (testResultAction) {
+        return """ \
+                    ${call()}
+                    Details: ${testResultAction.urlName}
+               """.stripIndent().trim()
+    }
+    return "No tests found"
+}
+
 def call() {
     Run build = $build()
     def testResultAction = build.getAction(AbstractTestResultAction)
