f791cbeda1a862c3652c30b09ee94b5e5d897a2f
Nikolay Arhipov <nikolay@agilestacks.com>
2018-05-08 14:46:35 +0300

    Moved parseHubOutputs to public Jenkins

---
+++ vars/parseHubOutputs.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call(def log) {
+    def paramFormat = /\d{4}(?:\/\d{2}){2} \d{2}(?::\d{2}){2}\s+(?:\[.*\]){0,1}\s([\d\w-_\.]+:[\d\w-_\.]+) = (.*)/
+
+    return log.split('Stack outputs:')[1].split('\n')
+            .collect { it =~ paramFormat }
+            .findAll { it.matches() }
+            .collectEntries { [(it.group(1)):it.group(2)] }
+}
