bf8795bd3afb2bc3d8f0845c87be694791badf89
Nikolay Arhipov <nikolay@agilestacks.com>
2018-05-10 18:19:05 +0300

    Tokenize instead of split

--- vars/parseHubOutputs.groovy
+++ vars/parseHubOutputs.groovy
@@ -3,7 +3,7 @@
 def call(def log) {
     def paramFormat = /\d{4}(?:\/\d{2}){2} \d{2}(?::\d{2}){2}\s+(?:\[.*\]){0,1}\s([\d\w-_\.]+:[\d\w-_\.]+) = (.*)/
 
-    return log.split('Stack outputs:')[1].split('\n')
+    return log.tokenize('Stack outputs:')[1].tokenize('\n')
             .collect { it =~ paramFormat }
             .findAll { it.matches() }
             .collectEntries { [(it.group(1)):it.group(2)] }
