89d0526cc8a778a9e59b3d543386775d3701be7b
Nikolay Arhipov <nikolay@agilestacks.com>
2018-05-10 11:41:39 +0300

    assumeDeployerRole lib

---
+++ vars/assumeDeployerRole.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy @GrabResolver(name='central', root='http://central.maven.org/maven2/') @GrabResolver(name = 'central', root = 'http://central.maven.org/maven2/')
+@Grab(group = 'com.amazonaws', module = 'aws-java-sdk', version = '1.11.313')
+@Grab(group = 'software.amazon.ion', module = 'ion-java', version = '1.0.2')
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider
+
+def call(String arn = null) {
+    def creds = new STSAssumeRoleSessionCredentialsProvider.Builder(arn, "jenkins-deployer")
+            .getCredentials()
+
+    return [
+            accessKey   : creds.getAWSAccessKeyId(),
+            secretKey   : creds.getAWSSecretKey(),
+            sessionToken: creds.getSessionToken()
+    ]
+
+}
