3ea48037f1db979ba252b6915624d9d1495dd56b
Nikolay Arhipov <nikolay@agilestacks.com>
2018-05-11 13:56:04 +0300

    Fixed parsehubOutputs

--- vars/parseHubOutputs.groovy
+++ vars/parseHubOutputs.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(def log) {
-    def paramFormat = /\d{4}(?:\/\d{2}){2} \d{2}(?::\d{2}){2}\s+(?:\[.*\]){0,1}\s([\d\w-_\.]+:[\d\w-_\.]+) = (.*)/
+    def paramFormat = /\d{4}(?:\/\d{2}){2} \d{2}(?::\d{2}){2}\s+(?:\[.*\]){0,1}\s([\d\w-_\.]+:[\d\w-_\.]+) => `(.*)`/
 
-    return log.tokenize('Stack outputs:')[1].tokenize('\n')
+    return log.split('Stack outputs:', 2)[1]
+            .tokenize('\n')
             .collect { it =~ paramFormat }
             .findAll { it.matches() }
             .collectEntries { [(it.group(1)):it.group(2)] }
