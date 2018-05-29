34fad1ce42090eeb3c524f16f6a0fa7a6128bd87
Antons Kranga <anton@agilestacks.com>
2018-05-29 16:41:21 +0300

    delete credentials script

--- resources/credentials/delete.groovy
+++ resources/credentials/delete.groovy
@@ -1,4 +1,35 @@
 package credentials
 
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.domains.Domain
+
+def DOMAIN      = '{{spec.domain}}' ?: null
+def CREDS_ID    = '{{spec.id}}' ?: '{{metadata.name}}' ?: null
+def SCOPE       = '{{spec.scope}}' ?: 'GLOBAL'
+
+def scope = SCOPE.toUpperCase() as CredentialsScope
+
+def creds = SystemCredentialsProvider.instance
+def domain = Domain.global()
+if (DOMAIN != 'global') {
+    domain = creds.
+                domainCredentialsMap.
+                keySet().
+                find {it.name == DOMAIN }
+    if (!domain) {
+        throw new IllegalArgumentException("Domain ${DOMAIN} has not been found")
+    }
+} else {
+    println "Use global domain"
+}
+
+def existing = creds.domainCredentialsMap.get(domain)?.find {it.id == CREDS_ID}
+if (!existing) {
+    throw new IllegalArgumentException("Credentials ${CREDS_ID}@${DOMAIN} has not been found")
+}
+
+creds.removeCredentials(domain, existing)
+
 println 'Not yet implemented'
 println 'Status: CONVERGED <EOF>'
