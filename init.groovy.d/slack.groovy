2e295e06d8b094c41771dea69bd7f8cc1145559e
Antons Kranga <anton@agilestacks.com>
2018-06-18 18:00:59 +0300

    Missing elvis operator (Fixes #1)

--- init.groovy.d/slack.groovy
+++ init.groovy.d/slack.groovy
@@ -32,7 +32,7 @@ def configmap = client.
         withLabels([
                 project: 'jenkins',
                 qualifier: 'slack'
-        ]).list().items.first()?.data
+        ]).list().items?.first()?.data
 
 println configmap
 
