a979b99136db4edec12f41dff485ef2f1f79241f
Antons Kranga <anton@agilestacks.com>
2018-06-19 19:36:38 +0300

    unified format for EnvVar spec test

--- test/com/agilestacks/jenkins/operator/resources/EnvVarsSpec.groovy
+++ test/com/agilestacks/jenkins/operator/resources/EnvVarsSpec.groovy
@@ -1,24 +1,48 @@
 package com.agilestacks.jenkins.operator.resources
 
-import com.agilestacks.jenkins.operator.resources.EnvVars
+import com.agilestacks.jenkins.operator.util.BasicScriptableRoutines
 import spock.lang.Specification
 
-class EnvVarsSpec extends Specification {
+class EnvVarsSpec extends Specification implements BasicScriptableRoutines<EnvVars> {
 
-    def "should merge option accessible from defaults"() {
-        when:
-            def resource = new EnvVars()
-        then:
-            resource.mergedWithDefaults.spec.merge == 'ours'
+    def "all scripts accessible and contains a magic string"() {
+        given:
+        def resource = new EnvVars()
+
+        expect:
+        resource.createScriptFile != null
+        resource.deleteScriptFile != null
+        resource.createScript != null
+        resource.deleteScript != null
+        resource.createScript =~ MAGIC_STRING
+        resource.deleteScript =~ MAGIC_STRING
     }
 
 
     def "should defaults should be overriden"() {
         given:
-            def resource = new EnvVars()
+        def resource1 = new EnvVars()
+        def resource2 = new EnvVars()
         when:
-            resource.spec.merge = 'their'
+        resource2.spec.merge = 'their'
+
         then:
-            resource.mergedWithDefaults.spec.merge == 'their'
+        resource1.mergedWithDefaults.spec.merge == 'ours'
+        resource2.mergedWithDefaults.spec.merge == 'their'
+    }
+
+
+    def "contains envvars as properties encoded base64"() {
+        given:
+        def resource = fromYaml('/envVars1.yaml', EnvVars)
+
+        when:
+        def encoded = resource.getMergedWithDefaults().variablesBase64 as String
+        def props = new Properties()
+        props.load(new ByteArrayInputStream( encoded.decodeBase64() ))
+
+        then:
+        props.URL_VAR == 'https://google.com'
+        props.STRING_VAR == 'hello, world!'
     }
 }
