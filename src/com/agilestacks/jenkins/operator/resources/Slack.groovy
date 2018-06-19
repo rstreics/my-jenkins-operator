f482f28264d238411994d36cb5b16a2d0ebae257
Antons Kranga <anton@agilestacks.com>
2018-06-19 20:17:08 +0300

    oopsie forgot logging

--- src/com/agilestacks/jenkins/operator/resources/Slack.groovy
+++ src/com/agilestacks/jenkins/operator/resources/Slack.groovy
@@ -1,8 +1,10 @@
 package com.agilestacks.jenkins.operator.resources
 
 import com.agilestacks.jenkins.operator.util.ScriptableResource
+import groovy.util.logging.Log
 import io.fabric8.kubernetes.client.CustomResource
 
+@Log
 class Slack extends CustomResource implements ScriptableResource {
     final String definitionFile   = '/slack/definition.yaml'
     final String createScriptFile = '/slack/create.groovy'
