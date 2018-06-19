fc4394092fd1b7ad4b891db61cb08908d7f1e124
Antons Kranga <anton@agilestacks.com>
2018-06-19 19:34:07 +0300

    clean up

--- test/com/agilestacks/jenkins/operator/util/BasicScriptableRoutines.groovy
+++ test/com/agilestacks/jenkins/operator/util/BasicScriptableRoutines.groovy
@@ -6,11 +6,11 @@ import io.fabric8.kubernetes.client.utils.Serialization
 trait BasicScriptableRoutines<T extends ScriptableResource> {
     static final MAGIC_STRING = /(?i)\s*print(ln)?\s*[(]?\s*['"]Status:\s+CONVERGED\s*<EOF>\s*['"]\s*[)]?\s*[;]?\s*/
 
-    def hasDefinitionAndScripts(T rsc) {
+    boolean hasDefinitionAndScripts(T rsc) {
         rsc.definition && rsc.createScript && rsc.deleteScript
     }
 
-    def fromYaml(String filepath, Class<T> clazz) {
+    T fromYaml(String filepath, Class<T> clazz) {
         final mapper = Serialization.yamlMapper()
         def text = this.class.getResourceAsStream(filepath).text
         mapper.readValue(text, clazz)
