e2a3f7ff5014b754abe967bdb4352b767d477a63
Antons Kranga <anton@agilestacks.com>
2018-07-02 08:59:12 +0300

    bound to explain output format

--- src/com/agilestacks/jenkins/share/HubExplanation.groovy
+++ src/com/agilestacks/jenkins/share/HubExplanation.groovy
@@ -7,34 +7,15 @@ class HubExplanation {
     Map<String, ?> explain
 
     HubExplanation(String json) {
-        explain = new JsonSlurper().parseText(json) as Map
+        this( new JsonSlurper().parseText(json) as Map )
     }
 
-    Map<String, String> getStackOutputs() {
-        return explain.stackOutputs as Map
+    HubExplanation(Map<String, ?> state) {
+        explain = state
     }
 
-    Map<String, String> getStackParameters() {
-        return explain.stackParameters as Map
-    }
-
-    String [] getProvides() {
-        return (explain.provides as Map).keySet()
-    }
-
-    Map<String, String> getComponentOutputs(String component) {
-        explain.components[(component)]?.outputs as Map
-    }
-
-    Map<String, String> getComponentParameters(String component) {
-        explain.components[(component)]?.parameters as Map
-    }
-
-    Map<String, String> componentOutputs(String component) {
-        getComponentOutputs(component)
-    }
-
-    Map<String, String> componentParameters(String component) {
-        getComponentParameters(component)
+    @Override
+    Object getProperty(String propertyName) {
+        explain.get(propertyName) ?: super.getProperty(propertyName)
     }
 }
