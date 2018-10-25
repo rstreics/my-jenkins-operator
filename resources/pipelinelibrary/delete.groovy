77d7949de8ff339fb42c77c5189df434d62906ad
Antons Kranga <anton@agilestacks.com>
2018-10-25 15:22:02 +0300

    Fixed typo in groovy script

--- resources/pipelinelibrary/delete.groovy
+++ resources/pipelinelibrary/delete.groovy
@@ -6,6 +6,6 @@ final NAME = '{{spec.name}}' ?: '{{metadata.name}}' ?: null
 
 println "Removing pipeline library ${NAME}"
 
-GlobalLibraries.get().libraries.removeAl { it.name == NAME }
+GlobalLibraries.get().libraries.removeAll { it.name == NAME }
 
 println 'Status: CONVERGED <EOF>'
